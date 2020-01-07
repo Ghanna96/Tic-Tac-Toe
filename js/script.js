a0853b0d7faea30c0ae0b3c679b20f21ab8837b1
Ghanna96 <akramghanna@outlook.com>
2020-01-07 13:16:49 +0100

    getWinner function

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const newPlayer = (name,mark)=>{
 
 const player1= newPlayer('Player1','X');
 const player2= newPlayer('Player2','O');
+const winComb=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 const gameBoard= (()=>{
     let gBoard=new Array(9);
@@ -18,7 +19,14 @@ const gameBoard= (()=>{
             squares[i].appendChild(el);
         }
     };
+
     const getTurn= ()=> turn;
+
+    const getMarkIndexes= (mark)=> gBoard.reduce(function(a, e, i) { 
+        if (e === mark)
+            a.push(i);
+        return a;
+    }, []);
 
     const playerTurn=()=>{
         turn= (turn ==0)? 1: 0;
@@ -26,20 +34,36 @@ const gameBoard= (()=>{
 
     function updateElement(i,n){ //update gBoard
         gBoard[i]=n;
+        console.log(gBoard);
     };
+    let checker = (arr, target) => target.every(v => arr.includes(v));
+
+    const getWinner= ()=>{
+        let x= getMarkIndexes("X");
+        let o= getMarkIndexes("O");
+        for(let i in winComb) {
+            if(checker(x,winComb[i])){
+                return "Player1";    
+            }else if(checker(o,winComb[i])){
+                return "Player2";
+            }
+        }
+        return false
+    }
 
     return{
         display,
         updateElement,
         playerTurn,
-        getTurn
+        getTurn,
+        getWinner
     }
 })();
 
 
 const domManage = (()=>{
 
-    function clickBox(){
+    function clickBox(){ //function called when clicking box
         let box= this;
         if(box.textContent==""){
             if(gameBoard.getTurn()==0){
@@ -51,7 +75,6 @@ const domManage = (()=>{
             let index= box.getAttribute('data-id');
             gameBoard.updateElement(index,box.textContent);
         }else {return}
-        //console.table(gBoard);
     }
 
     const addEventClick= ()=>{
