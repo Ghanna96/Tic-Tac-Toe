d13c18d67dc915c72ae5419a897fd3e31a6c4962
Ghanna96 <akramghanna@outlook.com>
2020-01-08 19:02:06 +0100

    remove event listeners after game's finished

--- js/script.js
+++ js/script.js
@@ -8,6 +8,40 @@ const player1= newPlayer('Player1','X');
 const player2= newPlayer('Player2','O');
 const winComb=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
+const domManage = (()=>{
+    function clickBox(){ //function called when clicking box
+        let box= this;
+        if(box.textContent==""){
+            if(gameBoard.getTurn()==0){
+                box.textContent=player1.mark;
+            }else if(gameBoard.getTurn()==1){
+                box.textContent=player2.mark;
+            }
+            gameBoard.playerTurn();
+            let index= box.getAttribute('data-id');
+            gameBoard.updateElement(index,box.textContent);
+            if(gameBoard.getWinner()){
+                alert(gameBoard.getWinner());
+            }
+        }else {return}
+    }
+
+    const addEventClick= ()=>{
+        squares.forEach((box)=>{
+            box.addEventListener('click',clickBox);
+        })
+    };
+    addEventClick();
+    const removeEventClick=()=>{
+        squares.forEach((box)=>{
+            box.removeEventListener('click',clickBox);
+        })
+    };
+    return{
+        removeEventClick
+    }
+})();
+
 const gameBoard= (()=>{
     let gBoard=new Array(9);
     let turn=0;
@@ -16,11 +50,6 @@ const gameBoard= (()=>{
     const display= (b)=>{
         document.querySelector("#board").classList.toggle('hide');
         b.style.display="none";
-        // this.style.display="none";
-        // for(let i in gBoard){
-        //     let el = document.createTextNode(`${gBoard[i]}`);
-        //     squares[i].appendChild(el);
-        // }
     };
 
     const getTurn= ()=> turn;
@@ -44,15 +73,19 @@ const gameBoard= (()=>{
     const getWinner= ()=>{
         let x= getMarkIndexes("X");
         let o= getMarkIndexes("O");
-        if(x.length+o.length==9){
-            return "It's a tie!"
-        }
         for(let i in winComb) {
             if(checker(x,winComb[i])){
-                return "Player1 wins!";    
+                domManage.removeEventClick();
+                return "Player1 wins!";
+                
             }else if(checker(o,winComb[i])){
+                domManage.removeEventClick();
                 return "Player2 wins!";
             }
+        }
+        if(x.length+o.length==9){
+            domManage.removeEventClick();
+            return "It's a tie!"
         }
         return false
     }
@@ -67,33 +100,7 @@ const gameBoard= (()=>{
 })();
 
 
-const domManage = (()=>{
 
-    function clickBox(){ //function called when clicking box
-        let box= this;
-        if(box.textContent==""){
-            if(gameBoard.getTurn()==0){
-                box.textContent=player1.mark;
-            }else if(gameBoard.getTurn()==1){
-                box.textContent=player2.mark;
-            }
-            gameBoard.playerTurn();
-            let index= box.getAttribute('data-id');
-            gameBoard.updateElement(index,box.textContent);
-            if(gameBoard.getWinner()){
-                alert(gameBoard.getWinner());
-            }
-        }else {return}
-    }
-
-    const addEventClick= ()=>{
-        squares.forEach((box)=>{
-            box.addEventListener('click',clickBox)
-        })
-    };
-    addEventClick();
-
-})();
 
 //gameBoard.display();
 // players.filter((x)=> x.played==false)
